ae6c634c6f71c9601e195e01cb9ab49df3f0b9f4
Billy Ferrara <willfira717@gmail.com>
2020-07-14 20:33:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,7 +49,7 @@ class Student
     sql = <<-SQL
     SELECT *
     FROM students
-    WHERE grade > 12
+    WHERE grade < 12
     SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
